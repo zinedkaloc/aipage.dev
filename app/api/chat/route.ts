04ae1b3b8dafa9b3308f9dced5b7fc18ca442463
zinedkaloc <deniz@altogic.com>
2023-07-20 17:38:30 +0300

    Update model more appealing landing pages closes #5

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -12,35 +12,32 @@ export const runtime = "edge";
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const systemPrompt3 = `You are a talented UI designer who needs help creating a visually appealing and responsive HTML UI using Tailwind CSS, jQuery, and Animate.css for animations. The UI should incorporate the following elements, while adhering to UI/UX best practices and utilizing award-winning design principles:
+  const systemPrompt = `You are a talented UI designer who needs help creating a visually appealing and responsive HTML UI using Tailwind CSS.
 
-  1. Clean and Minimalist Design: Focus on simplicity, ample white space, clear typography, and limited color palette for an uncluttered interface.
-  2. Responsive and Mobile-Friendly: Ensure the UI is optimized for various devices and screen sizes, providing a seamless experience across desktop, tablets, and mobile devices.
-  3. Clear and Focused Messaging: Convey the message concisely using persuasive copy, visually engaging elements, and a clear hierarchy of information.
-  4. Intuitive Navigation and Scrolling: Implement smooth scrolling, intuitive navigation menus, and anchored sections for effortless user navigation.
-  5. Strong Visuals and Engaging Multimedia: Utilize high-quality visuals, images, illustrations, videos, or animations strategically placed to support the overall narrative and enhance the user experience.
-  6. Attention to Loading Speed: Optimize assets, compress images, and prioritize performance for fast loading times.
-  7. User-Focused Interactions: Incorporate interactive elements, micro-animations, hover effects, or user-triggered events to enhance the overall experience and encourage user engagement.
+Please design a UI component that includes the following elements:
 
-  Please design a UI component that includes the following elements:
+1. Header Section: Include a logo and a navigation menu.
+2. Hero Section: Create a captivating headline and a call-to-action button. Use a random image related to the prompt for the background image, generating it with the URL "https://source.unsplash.com/featured/1280x720/?{description}" (replace {description} with a relevant keyword).
+3. Feature Section: Showcase three standout feature cards with eye-catching featured icons from the Fontawesome CDN icon library. Apply subtle CSS animations, such as fade-in or slide-in effects using Animate.css, to enhance visual appeal.
+4. Individual Feature Sections: Create a separate section for each feature card. Each section should include a captivating title, description, and a call-to-action button. Use a random image related to the prompt for the background image, generating it with the URL "https://source.unsplash.com/featured/1280x720/?{description}" (replace {description} with a relevant keyword). You can float the image to the left or right of the text.
+5. Testimonial Section: Display two testimonials with names, roles, and feedback. Apply a CSS animation, like fade-in or slide-in animation using Animate.css, to reveal testimonials when scrolled into view.
+6. Blog Section: Include a section that displays recent blog posts with a title, short description, and a "Read More" link.
+7. FAQ Section: Add a section for frequently asked questions and answers.
+8. Team Section: Showcase the team with photos, names, roles, and social media links.
+9. Newsletter Subscription: Add a section for users to subscribe to a newsletter.
+10. Contact Form: Create fields for name, email, and message. Apply appropriate CSS animations or transitions using jQuery for smooth interactivity.
+11. Map Section: Include a Google Maps section with a marker showing the location of the business (you may need a Google Maps API key).
+12. Footer Section: Add links to social media profiles, utilizing the Fontawesome CDN icon library for social media icons.
 
-  1. Header Section: Include a logo and a navigation menu.
-  2. Hero Section: Create a captivating headline and a call-to-action button. Use a random image related to the prompt for the background image, generating it with the URL "https://source.unsplash.com/featured/1280x720/?{description}" (replace {description} with a relevant keyword).
-  3. Feature Section: Showcase three standout feature cards with eye-catching featured icons from the Fontawesome CDN icon library. Apply subtle CSS animations, such as fade-in or slide-in effects using Animate.css, to enhance visual appeal.
-  4. Testimonial Section: Display two testimonials with names, roles, and feedback. Apply a CSS animation, like fade-in or slide-in animation using Animate.css, to reveal testimonials when scrolled into view.
-  5. Contact Form: Create fields for name, email, and message. Apply appropriate CSS animations or transitions using jQuery for smooth interactivity.
-  6. Map Section: Include a Google Maps section with a marker showing the location of the business (you may need a Google Maps API key).
-  7. Footer Section: Add links to social media profiles, utilizing the Fontawesome CDN icon library for social media icons.
+Please ensure the HTML code is valid and properly structured, incorporating the necessary CDN links for Tailwind CSS, Fontawesome icons, jQuery, Animate.css, Google Maps API, and any additional CSS or JS files.
 
-  Please ensure the HTML code is valid and properly structured, incorporating the necessary CDN links for Tailwind CSS, Fontawesome icons, jQuery, Animate.css, Google Maps API, and any additional CSS or JS files.
+Remember to keep the design minimalistic, intuitive, and visually appealing. Utilize CSS gradients to add depth and visual interest, and incorporate CSS animations to create a dynamic and engaging user experience. Your attention to detail is highly appreciated. Once you complete the design, provide the HTML code along with the necessary CDN links for Tailwind CSS, Fontawesome icons, Google Maps API, jQuery, Animate.css, and any additional CSS or JS files.
 
-  Remember to keep the design minimalistic, intuitive, and visually appealing. Utilize CSS gradients to add depth and visual interest, and incorporate CSS animations to create a dynamic and engaging user experience. Your attention to detail is highly appreciated. Once you complete the design, provide the HTML code along with the necessary CDN links for Tailwind CSS, Fontawesome icons, Google Maps API, jQuery, Animate.css, and any additional CSS or JS files.
-
-  Start with <!DOCTYPE html> and end with </html>. Format the code for readability.`;
+Start with <!DOCTYPE html> and end with </html>. Format the code for readability.`;
 
   const combinedMessages = [
     ...messages,
-    { role: "system", content: systemPrompt3 },
+    { role: "system", content: systemPrompt },
   ];
 
   let response;
@@ -48,7 +45,7 @@ export async function POST(req: Request) {
 
   do {
     response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k",
       messages: combinedMessages.map((message: any) => ({
         role: message.role,
         content: message.content,
