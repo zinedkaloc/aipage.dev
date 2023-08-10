7c48dc10bed2411b4fd8657b8bb99e2369a83f5f
zinedkaloc <deniz@altogic.com>
2023-08-10 19:18:29 +0300

    Update rate limiter to 5

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,7 +9,7 @@ import { headers } from "next/headers";
 const ratelimit = redis
   ? new Ratelimit({
       redis: redis,
-      limiter: Ratelimit.fixedWindow(10, "1440 m"),
+      limiter: Ratelimit.fixedWindow(5, "1440 m"),
       analytics: true,
     })
   : undefined;
