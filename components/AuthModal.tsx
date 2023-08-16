92bb786c1a9421ac49abd528909aa53d0cb165ea
zinedkaloc <deniz@altogic.com>
2023-08-16 17:13:02 +0300

    Add padding to authmodal

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -33,7 +33,7 @@ export default function AuthModal() {
   }
 
   return (
-    <Modal close={close} isOpen={has("authModal")}>
+    <Modal close={close} isOpen={has("authModal")} className="p-0">
       <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-10">
         <Link href="/">
           <Image
