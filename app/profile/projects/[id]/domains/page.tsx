9d69ca9b5decbc600459ec53f8269f28d297a946
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2023-08-28 13:27:17 +0300

    fix: typescript error

--- app/profile/projects/[id]/domains/page.tsx
+++ app/profile/projects/[id]/domains/page.tsx
@@ -11,17 +11,7 @@ export default function ProjectDomains() {
     <div className="flex flex-col gap-4">
       {hasDomains &&
         toReversed(project?.domains).map((domain) => (
-          <DomainCard
-            domain={domain}
-            key={domain._id}
-            props={{
-              type: "rewrite",
-              primary: domain.isPrimary,
-              slug: domain.domain,
-              target: "https://google.com",
-              verified: domain.status === "Valid Configuration",
-            }}
-          />
+          <DomainCard domain={domain} key={domain._id} />
         ))}
     </div>
   );
