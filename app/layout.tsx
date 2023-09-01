3bfd84d4f82555fa756eaefbd7ca617525347e23
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2023-09-01 15:33:24 +0300

    add: Feature rendering pure html

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,21 @@
-import "../styles/globals.css";
-import { Inter } from "next/font/google";
-import { Metadata } from "next";
 import AuthModal from "@/components/AuthModal";
-import { AuthProvider } from "@/context/AuthContext";
-import { ReactNode } from "react";
-import { fetchAuthUser, fetchProducts, getProjectByDomain } from "@/utils/auth";
 import PricesModal from "@/components/PricesModal";
-import { headers } from "next/headers";
-import { isAipage } from "@/utils/helpers";
-import HTMLPreview from "@/components/HTMLPreview";
+import { AuthProvider } from "@/context/AuthContext";
 import { Project } from "@/types";
+import { fetchAuthUser, fetchProducts, getProjectByDomain } from "@/utils/auth";
+import { isAipage } from "@/utils/helpers";
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { headers } from "next/headers";
+import { ReactNode } from "react";
+import "../styles/globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export async function generateMetadata(): Promise<Metadata> {
   const isAipageDomain = isAipage(headers().get("Host") as string);
   if (!isAipageDomain) {
     const project = await getProjectByDomain(headers().get("Host") as string);
-    if (project) {
-      return {
-        title: `Preview - ${project.name ?? project.content} | AIPage.dev`,
-      };
-    }
+    if (project) return {};
   }
 
   return {
@@ -55,19 +50,17 @@ export default async function RootLayout({
     project = await getProjectByDomain(headers().get("Host") as string);
   }
 
+  if (!isAipageDomain && project?.result) {
+    return require("html-react-parser")(project?.result);
+  }
+
   return (
     <AuthProvider user={user ?? null}>
       <html lang="en">
         <body className={inter.className}>
-          {isAipageDomain || !project ? (
-            <>
-              {children}
-              <AuthModal />
-              <PricesModal products={products} />
-            </>
-          ) : (
-            <HTMLPreview html={project.result} />
-          )}
+          {children}
+          <AuthModal />
+          <PricesModal products={products} />
         </body>
       </html>
     </AuthProvider>
